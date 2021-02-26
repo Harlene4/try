7f03e4d16b4c22785ff435086917be6a35f8a3fd
Harlene4 <77616751+Harlene4@users.noreply.github.com>
2021-02-26 15:12:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <frameset src= "">
 <frameset src= "https://sleepy-springs-75293.herokuapp.com/">
 <frameset rows= "50%,  50%">
-<frame src= "https://youtube.com/watch?v=i0ZabxXmH4Y&feature=share">
-<frame src= "https://youtu.be/XCzzf4KGiM0">
+<frame src= "<iframe width="560" height="315" src="https://www.youtube.com/embed/i0ZabxXmH4Y" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>">
+<frame src= "<iframe width="560" height="315" src="https://www.youtube.com/embed/XCzzf4KGiM0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>">
 </frameset>
 </html>
