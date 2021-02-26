9aae0ec2ce21780df7f56a7a4ea7a384acc939f2
Harlene4 <77616751+Harlene4@users.noreply.github.com>
2021-02-26 15:18:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <html>
-<head> <TITLE> TLE PT </TITLE>
-</head>
-<framset cols= "50%, 50%>
-<frameset src= "">
-<frameset src= "https://sleepy-springs-75293.herokuapp.com/">
-<frameset rows= "50%,  50%">
-<frame src= "<iframe width="560" height="315" src="https://www.youtube.com/embed/i0ZabxXmH4Y" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>">
-<frame src= "<iframe width="560" height="315" src="https://www.youtube.com/embed/XCzzf4KGiM0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>">
-</frameset>
+  <head><title></title></head>
+  <body>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/i0ZabxXmH4Y" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </body>
 </html>
